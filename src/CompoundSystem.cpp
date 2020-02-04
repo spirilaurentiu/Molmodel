58da53cd90d495d91d11f7cfc2693a6e050af9f4
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2020-02-04 17:36:30 +0200

    Added FreeLine root mobility.

--- src/CompoundSystem.cpp
+++ src/CompoundSystem.cpp
@@ -586,6 +586,14 @@ void CompoundSystem::modelOneCompound(CompoundIndex compoundId, String mobilized
 					    Transform());
 		            unit.bodyId = weldBody.getMobilizedBodyIndex();
                     std::cout << " got Weld mobodIx " << unit.bodyId << std::endl;
+                }else if (mobilizedBodyType.compare("FreeLine") == 0) {
+                    MobilizedBody::FreeLine freeLineBody
+                            (matter.Ground(),
+                             G_X_T * T_X_B,
+                             dumm.calcClusterMassProperties(unit.clusterIx),
+                             Transform());
+                    unit.bodyId = freeLineBody.getMobilizedBodyIndex();
+                    std::cout << " got Weld mobodIx " << unit.bodyId << std::endl;
                 }
                 dumm.attachClusterToBody(unit.clusterIx, unit.bodyId);
             }
