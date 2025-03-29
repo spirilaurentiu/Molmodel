b0c41aa4ae8d5df87a3fe8fc66ecc337a8040ef9
Victor Gabriel Ungureanu <victorungu99@gmail.com>
2025-03-29 16:02:03 +0200

    re-added gbsa

--- src/OpenMMPlugin.cpp
+++ src/OpenMMPlugin.cpp
@@ -305,8 +305,8 @@ std::string OpenMMPluginInterface::initializeOpenMM(bool allowReferencePlatform,
         }
 
         // System takes over heap ownership of the force.
-        ///// openMMSystem->addForce(ommGBSAOBCForce.get()); ommGBSAOBCForce.release();
-        ///// std::cout << "OpenMMPlugin added GBSA scaled at" << dumm->gbsaGlobalScaleFactor << std::endl;
+        openMMSystem->addForce(ommGBSAOBCForce.get()); ommGBSAOBCForce.release();
+        std::cout << "OpenMMPlugin added GBSA scaled at" << dumm->gbsaGlobalScaleFactor << std::endl;
     }
 
     // Add bonded forces
